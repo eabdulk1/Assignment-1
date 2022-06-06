dfc92aa9c5dc968c53c61c6804b6a4c84510a525
Eabulkaliq <sam.abulkaliq@gmail.com>
2022-06-06 18:54:02 -0400

    this is my assignment#1

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+const path = require("path");
+// use res.render to load up an ejs view file
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+// index page
+app.get("/", function (req, res) {
+  
+  var tagline =
+    "CEO Of This Organization:";
+
+  res.render("pages/index", {
+    tagline: tagline,
+  });
+});
+
+// about page
+app.get("/about", function (req, res) {
+  res.render("pages/about");
+});
+
+app.get("/contact", function (req, res) {
+  res.render("pages/contact");
+});
+
+app.listen(8080);
+console.log("Server is listening on port 8080");
